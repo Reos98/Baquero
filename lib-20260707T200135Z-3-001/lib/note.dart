e071dd5b3aef0589c70ae69c5feb13e68833b8c2
Reos98 <bl.montoyab@uea.edu.ec>
2026-07-19 21:23:12 -0500

    Subiendo archivo del proyecto: lib-20260707T200135Z-3-001/lib/note.dart

---
+++ lib-20260707T200135Z-3-001/lib/note.dart
@@ -0,0 +1,26 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  final int createdAt;
+
+  Note({this.id, required this.title, required this.content, required this.createdAt});
+
+  Map<String, Object?> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'] as int?,
+      title: map['title'] as String,
+      content: map['content'] as String,
+      createdAt: map['createdAt'] as int,
+    );
+  }
+}
